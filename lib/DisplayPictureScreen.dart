8e7d95a3532966c25a4705697476a739e3b4f3fe
ziabric <kurbet78@list.ru>
2024-04-13 12:07:09 +0300

    minor

--- lib/DisplayPictureScreen.dart
+++ lib/DisplayPictureScreen.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
-import 'dart:html';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+
 
 class DisplayPictureScreen extends StatefulWidget {
   final XFile image;
@@ -18,9 +20,8 @@ class DisplayPictureScreen extends StatefulWidget {
 class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
   Uint8List? _imageBytes;
   Image? _image;
-  String _price = "";
-  String _category = "";
-  bool _localModelUseFlg = false;
+  bool _localModelUseFlag = false;
+  String _location = "";
 
 
   @override
@@ -36,14 +37,10 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
     });
   }
 
-  void _changeStateCheckbox(bool? flag) {
-    setState(() {
-      _localModelUseFlg = flag!;
-    });
-  }
-
-  Future<void> _getInfoFromModel() async {
+  Future<void> _sendReport() async {
     
+    await _updateLocation();
+
     Uri url = Uri.parse('http://127.0.0.1:8080/path');
 
     final body = base64Encode(_imageBytes!);
@@ -53,8 +50,7 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
       if (response.statusCode == 200) {
         print('Response body: ${response.body}');
         setState(() {
-          _price = "";
-          _category = "";
+
         });
       } else {
         print('Request failed with status: ${response.statusCode}.');
@@ -64,12 +60,50 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
     }
   }
 
-  Future<void> _getLocalInfoFromModel() async {
+  Future<void> _sendReportLater() async {
+    
+    await _updateLocation();
     
     try {
       
     } catch (e) {
       print('Exception caught: $e');
+    }
+  }
+
+  Future<void> _updateLocation() async {
+    try {
+      // Запрашиваем разрешение на использование геолокации
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          _location = 'Location permission denied';
+        }
+      }
+
+      if (permission == LocationPermission.deniedForever) {
+        // Права на использование геолокации навсегда отклонены
+        setState(() {
+          _location = 'Location permissions are permanently denied, we cannot request permissions.';
+        });
+      } 
+
+      // Получаем текущее местоположение пользователя
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+      // Преобразуем координаты в адрес
+      List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+      Placemark place = placemarks[0];
+      
+      // Формируем строку адреса
+      setState(() {
+        _location = "${place.street}, ${place.locality}, ${place.postalCode}, ${place.country}";
+      });
+      
+    } catch (e) {
+      // Обработка возможных исключений
+      print('Failed to get location: $e');
     }
   }
 
@@ -86,17 +120,11 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
       Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Checkbox(value: _localModelUseFlg, onChanged: _changeStateCheckbox,), 
-          const Text("Use local model"),
-        ],
-      ),
-      TextButton.icon(onPressed: (!_localModelUseFlg) ? _getInfoFromModel : _getLocalInfoFromModel, icon: const Icon(Icons.cloud_download), label: const Text("Get Info")),
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Price: $_price"),
-          Text("Category: $_category"),],
-      )
+        TextButton.icon(onPressed: _sendReport, icon: const Icon(Icons.email), label: const Text("Send report")),
+        TextButton.icon(onPressed: _sendReportLater, icon: const Icon(Icons.hourglass_full), label: const Text("Send report later")),
+      ],),
+      TextButton.icon(onPressed: _updateLocation, icon: const Icon(Icons.location_on), label: const Text("Update address")),
+      Text(_location)
     ];
 
     return Scaffold(
