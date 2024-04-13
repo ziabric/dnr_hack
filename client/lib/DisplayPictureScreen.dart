bff7478caae8c3fe9e7ccdeb43b4466e57c98050
ziabric <kurbet78@list.ru>
2024-04-14 02:48:26 +0300

    minor

--- client/lib/DisplayPictureScreen.dart
+++ client/lib/DisplayPictureScreen.dart
@@ -1,12 +1,12 @@
-import 'dart:convert';
-import 'dart:typed_data';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:http/http.dart' as http;
-import 'package:geolocator/geolocator.dart';
-import 'package:geocoding/geocoding.dart';
+import "dart:convert";
+import "dart:typed_data";
+import "package:flutter/cupertino.dart";
+import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
+import "package:image_picker/image_picker.dart";
+import "package:http/http.dart" as http;
+import "package:geolocator/geolocator.dart";
+import "package:geocoding/geocoding.dart";
 
 
 class DisplayPictureScreen extends StatefulWidget {
@@ -28,6 +28,55 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
   TextEditingController _address = TextEditingController();
   TextEditingController _building = TextEditingController();
   TextEditingController _postCode = TextEditingController();
+
+  String listString = '''г.Волноваха,    ул. Ленина,64-66
+  г.Волноваха,    ул. Ленина,58-62
+  г.Волноваха,    ул. Ленина,58-62
+  г.Волноваха,    ул. Ленина,58-62
+  г.Волноваха,    ул. Гагарина,2
+  г.Волноваха,    ул. Ленина,162
+  г.Волноваха,    ул. Ленина,147
+  г.Волноваха,ул.Вл.Жоги,52
+  город Волноваха, улица Донецкая, д. 274
+  с.Ровнополь,ул.Садовая,34
+  с.Ровнополь,ул.Садовая,55а
+  с.Кирилловка
+  с.Новоандреевка
+  пгт.Володарское, ул.Ленина,15
+  ПГТ.Володарское, ул.Ленина,79
+  с.Старченково, ул. Школьная,26
+  с.Екатериновка, ул.Ленина,46
+  с.Малоянисоль, ул.Греческая, 8
+  с.Малоянисоль, ул.Греческая, 8
+  пгт.Володарское, ул.Ленина, 85
+  с.Старченково, ул. Калинина, 27 в
+  с.Старченково, ул. Калинина, 27 в
+  пгт.Володарское, ул.Ленина,22
+  с.Малиновка, ул.Ленина, 6
+  пгт.Володарское, ул.Ленина, 22
+  пгт.Володарское, ул.Володарского, 127, 
+  пгт.Володарское, ул.Аэродромная,5
+  пгт.Володарское, ул.Ленина, 114
+  пгт.Володарское, ул.Ленина, 79 а
+  с.Тополиное, ул.Комсомольская,8
+  пгт.Володарское, ул.Ленина, 40 а
+  пгт.Володарское, ул.Ленина, 85в
+  пгт.Володарское, ул.Речная, 1в
+  с.Кальчик, ул. Центральная,53
+  пгт.Володарское, ул.Калинина, 54
+  с.Кальчик, ул. Советская,48
+  с.Македоновка, ул.Гагарина, 78
+  с.Республика, ул. Ленина, д.2 
+  с.Боевое, ул.Сенатосенко, д.17
+  с.Малоянисоль, ул.Димитрова, 12а
+  пгт.Володарское, ул.Ленина, 79/8
+  пгт.Володарское, ул.Ленина, 3 а
+  пгт.Володарское, ул.Ленина, 114
+  пгт.Володарское, ул.Пушкина, 131
+  пгт.Володарское, ул.Пушкина, 88 е
+  пгт.Володарское, ул.Калинина,141 А''';
+
+  List<String> list = <String>[];
 
   @override
   void initState() {
@@ -83,18 +132,7 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
 
   Future<void> _selectFromList() async {
 
-    final List<String> list = <String>[
-      "Москва, улица Тверская, дом 7, 125009",
-      "Санкт-Петербург, Невский проспект, дом 22, 191186",
-      "Новосибирск, улица Ленина, дом 12, 630099",
-      "Екатеринбург, проспект Ленина, дом 50, 620075",
-      "Казань, улица Баумана, дом 34, 420111",
-      "Нижний Новгород, улица Рождественская, дом 17, 603001",
-      "Челябинск, улица Свободы, дом 64, 454091",
-      "Омск, проспект Мира, дом 55, 644042",
-      "Ростов-на-Дону, проспект Буденновский, дом 1, 344002",
-      "Уфа, улица Ленина, дом 102, 450000"
-    ];
+    
 
 
     showDialog(context: context, builder: (context) {
@@ -124,17 +162,15 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
 
   Future<void> _sendReport() async {
 
-    Uri url = Uri.parse('http://46.34.144.174:8000/image-to-text/');
+    Uri url = Uri.parse("http://46.34.144.174:8000/image-to-text/");
 
     final body = base64Encode(_imageBytes!);
     
-    String bodyTmp = "{loc:\"$_location\", image_base64:\"$body\"}";
+    String bodyTmp = "{\"loc\":\"$_location\", \"image_base64\":\"$body\"}";
 
     try {
-      var response = await http.post(url, headers: {'Content-Type': 'application/json; charset=utf-8',}, body: bodyTmp);
+      var response = await http.post(url, headers: {"Content-Type": "application/json; charset=utf-8",}, body: bodyTmp);
       if (response.statusCode == 200) {
-        print('Response body: ${response.body}');
-
         showDialog(context: context, builder: 
             (context) {
               return Dialog(
@@ -143,7 +179,7 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Icon(Icons.thumb_up),
+                      const Icon(Icons.thumb_up, size: 60,),
                       Text(utf8.decode(response.bodyBytes)),
                       TextButton(onPressed: (){
                         Navigator.pop(context);
@@ -160,43 +196,39 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
 
         });
       } else {
-        print('Request failed with status: ${response.statusCode}.');
+        print("Request failed with status: ${response.statusCode}.");
       }
     } catch (e) {
-      print('Exception caught: $e');
+      print("Exception caught: $e");
     }
   }
 
   Future<void> _sendReportLater() async {
-    
     try {
-      
     } catch (e) {
-      print('Exception caught: $e');
+      print("Exception caught: $e");
     }
   }
 
   Future<void> _updateLocation() async {
-
     if (_city.text == "" || _address.text == "" || _building.text == "")
     {
       return;
     }
-
     try {
       // Запрашиваем разрешение на использование геолокации
       LocationPermission permission = await Geolocator.checkPermission();
       if (permission == LocationPermission.denied) {
         permission = await Geolocator.requestPermission();
         if (permission == LocationPermission.denied) {
-          _location = 'Location permission denied';
+          _location = "Location permission denied";
         }
       }
 
       if (permission == LocationPermission.deniedForever) {
         // Права на использование геолокации навсегда отклонены
         setState(() {
-          _location = 'Location permissions are permanently denied, we cannot request permissions.';
+          _location = "Location permissions are permanently denied, we cannot request permissions.";
         });
       } 
 
@@ -210,17 +242,22 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
       // Формируем строку адреса
       setState(() {
         _location = "${place.locality}, ${place.thoroughfare}, ${place.name}, ${place.postalCode}";
+        if ( list.contains(_location) ) {
+          _location = "";
+        }
       });
       
     } catch (e) {
       // Обработка возможных исключений
-      print('Failed to get location: $e');
+      print("Failed to get location: $e");
     }
   }
 
   @override
   Widget build(BuildContext context) {
     
+
+    list = listString.split('\n');
 
     final Size screenSize = MediaQuery.of(context).size;
     final double width = screenSize.width;
